f4053cd5176e7ad455184ecdf6d2262bfb2effec
Esequiel Linares <esequiellinares06@gmail.com>
2026-01-16 17:24:20 -0500

    removed something

--- our-story/src/app/(app)/page.tsx
+++ our-story/src/app/(app)/page.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
 import LandingPage from "../../../components/LandingPage";
+import { AnimatePresence } from "motion/react";
 
 export default function Home() {
   return (
     <div className="min-h-screen">
-      <LandingPage />
+      <AnimatePresence mode="wait">
+        <LandingPage />
+      </AnimatePresence>
     </div>
   );
 }
