b7d96db62a474d76986a1045e5f87becd3202901
Esequiel Linares <esequiellinares06@gmail.com>
2026-01-20 15:23:21 -0500

    done

--- src/models/Eric.tsx
+++ src/models/Eric.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import { useRef } from "react";
+import { useGLTF } from "@react-three/drei";
 
-const Eric = () => {
+const Eric = (props: any) => {
+  const ref = useRef<any>(null);
+  const { scene } = useGLTF("/birthday-eric.glb");
+
+
+
   return (
-    <div>Eric</div>
-  )
-}
+    <group ref={ref} {...props}>
+      <primitive object={scene} />
+    </group>
+  );
+};
 
-export default Eric
+export default Eric;
+
+useGLTF.preload("/birthday-eric.glb");
